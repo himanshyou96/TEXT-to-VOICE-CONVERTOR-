13cfa2fbfe6b7a9cd2ad73205766d8d8bbfd2dba
himanshyou96 <himanshudiwakar526@gmail.com>
2025-07-21 02:30:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
-let speech = new SpeechSynthesisUtterance();
-
+    let speech = new SpeechSynthesisUtterance();
 let voices = [];
-
 let voiceselect = document.querySelector("select");
 
-window.speechSynthesis.onvoiceschanged = () => {
+function populateVoices() {
     voices = window.speechSynthesis.getVoices();
+    if (voices.length === 0) return;
+
     speech.voice = voices[0];
+    voiceselect.innerHTML = ''; // clear old options
 
-   voices.forEach((voice, i)=> (voiceselect.options[i] = new Option(voice.name, i)));
-};
+    voices.forEach((voice, i) => {
+        const option = new Option(voice.name + ' (' + voice.lang + ')', i);
+        voiceselect.add(option);
+    });
+}
 
-voiceselect.addEventListener("change", () =>{
+// Try populating on page load
+populateVoices();
+
+// Try again when voices are loaded
+window.speechSynthesis.onvoiceschanged = populateVoices;
+
+voiceselect.addEventListener("change", () => {
     speech.voice = voices[voiceselect.value];
 });
 
-
-
-
-
-
-
-document.querySelector("button").addEventListener("click",() =>{ 
+document.querySelector("button").addEventListener("click", () => {
     speech.text = document.querySelector("textarea").value;
     window.speechSynthesis.speak(speech);
-})
+});
